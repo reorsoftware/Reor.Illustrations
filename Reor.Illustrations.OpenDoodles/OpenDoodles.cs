69cb239615d22717967513a0d5c8f919fc86d8da
jake <j.bailey-saltmarsh+github@outlook.com>
2026-04-30 01:16:46 +0100

    init

---
+++ Reor.Illustrations.OpenDoodles/OpenDoodles.cs
@@ -0,0 +1,45 @@
+// ReSharper disable once CheckNamespace
+
+using System.Drawing;
+
+namespace Reor.Illustrations;
+
+public static class OpenDoodles
+{
+    private static readonly Color _foreground = Color.FromArgb(0x00, 0x00, 0x00);
+    private static readonly Color _background = Color.FromArgb(0x00, 0xFF, 0xFF, 0xFF);
+    private static readonly Color _accent = Color.FromArgb(0xFF, 0x61, 0x61);
+
+    public static string Ballet => OpenDoodleSvgs.Ballet(_foreground, _background, _accent);
+    public static string Bikini => OpenDoodleSvgs.Bikini(_foreground, _background, _accent);
+    public static string Coffee => OpenDoodleSvgs.Coffee(_foreground, _background, _accent);
+    public static string Dancing => OpenDoodleSvgs.Dancing(_foreground, _background, _accent);
+    public static string DogJump => OpenDoodleSvgs.DogJump(_foreground, _background, _accent);
+    public static string Doogie => OpenDoodleSvgs.Doogie(_foreground, _background, _accent);
+    public static string Dumping => OpenDoodleSvgs.Dumping(_foreground, _background, _accent);
+    public static string Float => OpenDoodleSvgs.Float(_foreground, _background, _accent);
+    public static string Groovy => OpenDoodleSvgs.Groovy(_foreground, _background, _accent);
+    public static string GroovySitting => OpenDoodleSvgs.GroovySitting(_foreground, _background, _accent);
+    public static string IceCream => OpenDoodleSvgs.IceCream(_foreground, _background, _accent);
+    public static string Laying => OpenDoodleSvgs.Laying(_foreground, _background, _accent);
+    public static string Loving => OpenDoodleSvgs.Loving(_foreground, _background, _accent);
+    public static string Meditating => OpenDoodleSvgs.Meditating(_foreground, _background, _accent);
+    public static string Messy => OpenDoodleSvgs.Messy(_foreground, _background, _accent);
+    public static string Moshing => OpenDoodleSvgs.Moshing(_foreground, _background, _accent);
+    public static string Petting => OpenDoodleSvgs.Petting(_foreground, _background, _accent);
+    public static string Plant => OpenDoodleSvgs.Plant(_foreground, _background, _accent);
+    public static string Reading => OpenDoodleSvgs.Reading(_foreground, _background, _accent);
+    public static string ReadingSide => OpenDoodleSvgs.ReadingSide(_foreground, _background, _accent);
+    public static string RollerSkating => OpenDoodleSvgs.RollerSkating(_foreground, _background, _accent);
+    public static string Rolling => OpenDoodleSvgs.Rolling(_foreground, _background, _accent);
+    public static string Running => OpenDoodleSvgs.Running(_foreground, _background, _accent);
+    public static string Selfie => OpenDoodleSvgs.Selfie(_foreground, _background, _accent);
+    public static string SitReading => OpenDoodleSvgs.SitReading(_foreground, _background, _accent);
+    public static string Sitting => OpenDoodleSvgs.Sitting(_foreground, _background, _accent);
+    public static string Sleek => OpenDoodleSvgs.Sleek(_foreground, _background, _accent);
+    public static string Sprinting => OpenDoodleSvgs.Sprinting(_foreground, _background, _accent);
+    public static string Strolling => OpenDoodleSvgs.Strolling(_foreground, _background, _accent);
+    public static string Swinging => OpenDoodleSvgs.Swinging(_foreground, _background, _accent);
+    public static string Unboxing => OpenDoodleSvgs.Unboxing(_foreground, _background, _accent);
+    public static string Zombieing => OpenDoodleSvgs.Zombieing(_foreground, _background, _accent);
+}
